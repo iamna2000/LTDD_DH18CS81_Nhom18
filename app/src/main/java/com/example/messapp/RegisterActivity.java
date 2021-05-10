0522ba761fdb8628fb820bf000b66a7bb84cd44f
iamna2000 <50236920+iamna2000@users.noreply.github.com>
2021-05-10 17:58:15 +0700

    Register Login Main

---
+++ app/src/main/java/com/example/messapp/RegisterActivity.java
@@ -0,0 +1,93 @@
+package com.example.messapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextInputEditText username, email, password;
+    Button btn_register;
+
+    FirebaseAuth mAuth;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btn_register = findViewById(R.id.btn_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_username = username.getText().toString();
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+
+                if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_password) || TextUtils.isEmpty(txt_email) ){
+                    Toast.makeText(RegisterActivity.this, "Fill all information", Toast.LENGTH_SHORT).show();
+                }else if (txt_password.length() < 6){
+                    Toast.makeText(RegisterActivity.this, "Password must be at least 6 characters" , Toast.LENGTH_SHORT).show();
+                }else {
+                    register(txt_username, txt_email, txt_password);
+                }
+            }
+        });
+    }
+
+
+    private void register(String username, String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
+
+            FirebaseUser user = mAuth.getCurrentUser();
+
+            reference = FirebaseDatabase.getInstance().getReference("User").child(user.getUid());
+
+            if ( user != null ) {
+//
+                HashMap<String, Object> hashMap = new HashMap<>();
+                hashMap.put("username", username);
+                hashMap.put("email", email);
+                hashMap.put("id", user.getUid());
+
+                reference.push().setValue(hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    }
+                });
+            }
+            Toast.makeText(RegisterActivity.this, "Register Success !! ", Toast.LENGTH_SHORT).show();
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this, "FAILLLLLLLL !!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/messapp/StartActivity.java
@@ -0,0 +1,38 @@
+package com.example.messapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button register, login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( com.example.messapp.StartActivity.this, LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent (com.example.messapp.StartActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
