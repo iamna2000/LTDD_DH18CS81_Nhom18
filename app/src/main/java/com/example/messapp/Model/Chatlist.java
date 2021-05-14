e149004842eeee2d9d9bef2fe0de49d2b211b275
iamna2000 <50236920+iamna2000@users.noreply.github.com>
2021-05-15 03:07:55 +0700

    ok imageupload but fail message

---
+++ app/src/main/java/com/example/messapp/Model/Chatlist.java
@@ -0,0 +1,21 @@
+package com.example.messapp.Model;
+
+public class Chatlist {
+    public Chatlist(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String id;
+
+    public Chatlist(){
+
+    }
+}
